ea5d51dcc2af6a0a2da36f52afa8bc358e09c356
Krzysztof Olszewski <krzysztofjuzolszewski@gmail.com>
2025-12-04 20:36:51 +0100

    zmiany 2

--- main.py
+++ main.py
@@ -42,25 +42,7 @@ def roots_20(coef: np.ndarray) -> tuple[np.ndarray, np.ndarray] | None:
 
 
 def frob_a(coef: np.ndarray) -> np.ndarray | None:
-    """Funkcja służąca do wyznaczenia macierzy Frobeniusa na podstawie
-    współczynników jej wielomianu charakterystycznego:
-    w(x) = a_n*x^n + a_{n-1}*x^{n-1} + ... + a_2*x^2 + a_1*x + a_0
 
-    Testy wymagają poniższej definicji macierzy Frobeniusa (implementacja dla 
-    innych postaci nie jest zabroniona):
-    F = [[       0,        1,        0,   ...,            0],
-         [       0,        0,        1,   ...,            0],
-         [       0,        0,        0,   ...,            0],
-         [     ...,      ...,      ...,   ...,          ...],
-         [-a_0/a_n, -a_1/a_n, -a_2/a_n,   ..., -a_{n-1}/a_n]]
-
-    Args:
-        coef (np.narray): Wektor współczynników wielomianu (n,).
-
-    Returns:
-        (np.ndarray): Macierz Frobeniusa o rozmiarze (n,n).
-        Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
-    """
     if coef is None:
         return None
     if not isinstance(coef, np.ndarray):
@@ -69,26 +51,26 @@ def frob_a(coef: np.ndarray) -> np.ndarray | None:
         return None
     if coef.size < 2:
         return None
-    a_n = coef[0]
+    a_n = coef[-1]
     if a_n == 0:
         return None
     n = coef.size-1 
     F = np.zeros((n, n), dtype=float)
     for i in range(n-1):
-        F[i, i+1] = 1
-    F[-1, :] = -coef[1:] / a_n
+        F[i,i+1] = 1
+    F[-1, :] = -coef[:-1] / a_n
     return F
 
 def is_nonsingular(A: np.ndarray) -> bool | None:
-    """Funkcja sprawdzająca czy podana macierz NIE JEST singularna. Przy
-    implementacji należy pamiętać o definicji zera maszynowego.
+    if not isinstance(A, np.ndarray):
+        return None
+    if A.ndim != 2 or A.shape[0] != A.shape[1]:
+        return None
+    A = A.astype(float)
+    eps = np.finfo(float).eps
+    detA = np.linalg.det(A)
+    if np.isclose(detA, 0.0, atol=eps, rtol=0.0):
+        return False
+    else:
+        return True
 
-    Args:
-        A (np.ndarray): Macierz (n,n) do przetestowania.
-
-    Returns:
-        (bool): `True`, jeżeli macierz A nie jest singularna, w przeciwnym 
-            wypadku `False`.
-        Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
-    """
-    pass
